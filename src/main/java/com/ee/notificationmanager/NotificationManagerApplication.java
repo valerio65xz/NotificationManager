a258e89122fa464c302f639e3125111293fb9923
Valerio Bondi <vale-bond@hotmail.it>
2022-11-10 11:46:48 +0100

    project created

---
+++ src/main/java/com/ee/notificationmanager/NotificationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ee.notificationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ee/notificationmanager/NotificationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ee.notificationmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
